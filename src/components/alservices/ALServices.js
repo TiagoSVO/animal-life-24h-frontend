f3fc6a7ae0fe15e1b9c11e51262d47d9c32d20a9
Tiago da Silva Valeriano de Oliveira <tiago.oliveira@ifb.edu.br>
2021-09-09 22:12:49 -0300

    [FIX] Ajustado responsividade do ALServices

--- src/components/alservices/ALServices.js
+++ src/components/alservices/ALServices.js
@@ -15,66 +15,32 @@ export default class ALServices extends Component {
                     <div className="al-services-title text-center">
                             <h1>Serviços</h1>
                         </div>
-                        <div className="al-services-content container">
-                            <div className="row d-flex justify-content-between text-center">
-                                <div className="col-sm-2 col-lg-3 al-service-card">
-                                    <div className="al-hexagon-wrap">
-                                        <div className="al-hexagon-img" style={{backgroundImage: 'url(https://picsum.photos/id/87/200)'}} ></div>
-                                    </div>
-                                    <div className="al-services-content">
-                                        <h3>Título</h3>
-                                        <p>Lorem ipsum dolor sit amet, 
-                                            consectetuer adipiscing elit, 
-                                            sed diam nonummy nibh euismod 
-                                            ncidunt ut laoreet dolore magna 
-                                            aliquam erat volutpat. Ut wisi 
-                                            enim ad minim veniam, quis nostrud 
-                                            exerci taron ullamcorper suscipit 
-                                            loboris nisl ut aliquip ex ea 
-                                            commodo consequat. Duis autem vel 
-                                            eum iriure dolor in hendrerit in 
-                                            vulputate velit esse molestie.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="col-sm-2 col-lg-3 al-service-card">
-                                    <div className="al-hexagon-wrap">
-                                        <div className="al-hexagon-img" style={{backgroundImage: 'url(https://picsum.photos/id/87/200)'}} ></div>
-                                    </div>
-                                    <div className="al-services-content">
-                                        <h3>Título</h3>
-                                        <p>Lorem ipsum dolor sit amet, 
-                                            consectetuer adipiscing elit, 
-                                            sed diam nonummy nibh euismod 
-                                            ncidunt ut laoreet dolore magna 
-                                            aliquam erat volutpat. Ut wisi 
-                                            enim ad minim veniam, quis nostrud 
-                                            exerci taron ullamcorper suscipit 
-                                            loboris nisl ut aliquip ex ea 
-                                            commodo consequat. Duis autem vel 
-                                            eum iriure dolor in hendrerit in 
-                                            vulputate velit esse molestie.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="col-sm-2 col-lg-3 al-service-card">
-                                    <div className="al-hexagon-wrap">
-                                        <div className="al-hexagon-img" style={{backgroundImage: 'url(https://picsum.photos/id/87/200)'}} ></div>
-                                    </div>
-                                    <div className="al-services-content">
-                                        <h3>Título</h3>
-                                        <p>Lorem ipsum dolor sit amet, 
-                                            consectetuer adipiscing elit, 
-                                            sed diam nonummy nibh euismod 
-                                            ncidunt ut laoreet dolore magna 
-                                            aliquam erat volutpat. Ut wisi 
-                                            enim ad minim veniam, quis nostrud 
-                                            exerci taron ullamcorper suscipit 
-                                            loboris nisl ut aliquip ex ea 
-                                            commodo consequat. Duis autem vel 
-                                            eum iriure dolor in hendrerit in 
-                                            vulputate velit esse molestie.
-                                        </p>
+                        <div className="al-services-content">
+                            <div className="container">
+                                <div className="row justify-content-between text-center">
+                                    <div className="col-sm-2 col-md-3">
+                                        <div className="d-flex">
+                                            <div className="al-service-card">
+                                                <div className="al-hexagon-wrap">
+                                                    <div className="al-hexagon-img" style={{backgroundImage: 'url(https://picsum.photos/id/87/200)'}} ></div>
+                                                </div>
+                                                <div className="al-services-content">
+                                                    <h3>Título</h3>
+                                                    <p>Lorem ipsum dolor sit amet, 
+                                                        consectetuer adipiscing elit, 
+                                                        sed diam nonummy nibh euismod 
+                                                        ncidunt ut laoreet dolore magna 
+                                                        aliquam erat volutpat. Ut wisi 
+                                                        enim ad minim veniam, quis nostrud 
+                                                        exerci taron ullamcorper suscipit 
+                                                        loboris nisl ut aliquip ex ea 
+                                                        commodo consequat. Duis autem vel 
+                                                        eum iriure dolor in hendrerit in 
+                                                        vulputate velit esse molestie.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
